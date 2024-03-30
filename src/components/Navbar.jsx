4cccd937ecc2b59bdc27dc4e9f9c49e52374033f
Sebastian Acuña <sebas2594@gmail.com>
2024-03-30 17:10:26 -0500

    se agregaron funcionalidades como menú y navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,39 @@
-
-
 import { MdKeyboardArrowDown } from "react-icons/md";
 import Avatar from './Avatar';
 import NavBarRight from './NavbarRight';
 import NavBarLeft from './NavBarLeft';
+import { useState } from "react";
+import NavBarArrowDown from "./NavbarArrowDown";
 
 const Navbar = () => {
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+    const showMenu = () => {
+        setIsOpen(!isOpen);
+
+    }
+
+    return (
         <>
             <div className="NavBar box-border p-5 flex justify-between overflow-hidden border-b border-gray-300">
-                <div className="NavBar-left flex row-span-1 sm:dis">                                                      
-                    <NavBarLeft/>
+                <div className="NavBar-left flex row-span-1 sm:dis">
+                    <NavBarLeft />
                 </div>
                 <div className="NavBar-right">
                     <div className='flex row'>
-                        <NavBarRight/>                    
-                        <Avatar/>                    
-                        <a className="mx-3 mt-2">
-                            <MdKeyboardArrowDown size={25}/>
+                        <NavBarRight />
+                        <Avatar />
+                        <a className="mx-3 mt-2 " onClick={showMenu} >
+                            <MdKeyboardArrowDown size={25} />
+                            {isOpen && (
+                                <div className="absolute top-0 right-0 mt-16 mr-10 w-40 shadow-lg z-50 mx-5 rounded-lg 0 backdrop-blur-xl border border-zinc-500">
+                                    <NavBarArrowDown/>
+                                </div>   
+                            )}
                         </a>
                     </div>
                 </div>
+
+                {!isOpen && console.log("menu cerrado")}
             </div>
         </>
     );
